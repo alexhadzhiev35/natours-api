de25c0f28075a6644bebb42ac47ef713ee94c519
op7icx <alexhadzhiev35@gmail.com>
2023-08-25 10:48:59 +0300

    Section 9 Finished: Error Handling

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -17,12 +22,18 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    // console.log(con.connections);
     console.log('DB connection successful!');
-    // console.log()
   });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
