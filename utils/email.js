7bdb1299456558c432422918506499b10d35365d
op7icx <alexhadzhiev35@gmail.com>
2023-08-27 12:07:54 +0300

    Section 10 Finished and congig.env ignored

---
+++ utils/email.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (option) => {
+  // 1)Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  // 2)Define the email options
+  const mailOptions = {
+    from: 'Jonas Schmedtmann <hello@jonas.io>',
+    to: option.email,
+    subject: option.subject,
+    text: option.message,
+    // html:
+  };
+  // 3)Actually send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
